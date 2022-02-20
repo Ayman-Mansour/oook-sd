8c23fa7a166325763c2b8dc65ae2ca4867e2e3be
Ayman Mansour <aymanmnsor777@gmail.com>
2022-02-20 11:24:50 +0300

    adding Google analytics

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,8 +1,32 @@
 import { CSSReset, ThemeProvider } from "@chakra-ui/core";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import theme from "../theme";
+import * as gtag from "../utils/analytics";
 
 
 function MyApp({ Component, pageProps }: any) {
+  useEffect(() => {
+    document.body.classList?.remove('loading');
+}, []);
+
+const router = useRouter();
+
+useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+        gtag.pageview(url);
+    };
+    router.events.on(
+        'routeChangeComplete',
+        handleRouteChange
+    );
+    return () => {
+        router.events.off(
+            'routeChangeComplete',
+            handleRouteChange
+        );
+    };
+}, [router.events]);
   return (
     <ThemeProvider theme={theme}>
       <CSSReset />
